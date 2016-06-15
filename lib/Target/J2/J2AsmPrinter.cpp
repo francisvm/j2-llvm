2b8159841ae91de80947a6993b39207e4c81ad8f
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-06-15 23:33:29 +0200

    j2: add MCInstLower
    * lib/Target/J2/CMakeLists.txt: Add J2MCInstLower.
    * lib/Target/J2/J2AsmPrinter.cpp,
    * lib/Target/J2/J2AsmPrinter.h: Use InstLower to lower instructions to
      MCInst and stream them.
    * lib/Target/J2/J2MCInstLower.cpp,
    * lib/Target/J2/J2MCInstLower.h: Lower operands.

--- lib/Target/J2/J2AsmPrinter.cpp
+++ lib/Target/J2/J2AsmPrinter.cpp
@@ -13,11 +13,20 @@
 
 #include "J2AsmPrinter.h"
 #include "InstPrinter/J2InstPrinter.h"
+#include "J2MCInstLower.h"
 #include "J2TargetMachine.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/Support/TargetRegistry.h"
 
 using namespace llvm;
+
+void J2AsmPrinter::EmitInstruction(const MachineInstr *MI) {
+  J2MCInstLower InstLower{OutContext, *this};
+
+  MCInst TmpInst;
+  InstLower.Lower(MI, TmpInst);
+  EmitToStreamer(*OutStreamer, TmpInst);
+}
 
 void J2AsmPrinter::EmitFunctionEntryLabel() {
   OutStreamer->EmitLabel(CurrentFnSym);
---
+++ lib/Target/J2/J2MCInstLower.h
@@ -0,0 +1,39 @@
+//===-- J2MCInstLower.h - Lower MachineInstr to MCInst ---------*- C++ -*--===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_J2_J2MCINSTLOWER_H
+#define LLVM_LIB_TARGET_J2_J2MCINSTLOWER_H
+
+#include "llvm/CodeGen/MachineOperand.h"
+#include "llvm/Support/Compiler.h"
+
+namespace llvm {
+class MCContext;
+class MCInst;
+class MCOperand;
+class MachineInstr;
+class AsmPrinter;
+
+/// J2MCInstLower - This class is used to lower an MachineInstr into an MCInst.
+class LLVM_LIBRARY_VISIBILITY J2MCInstLower {
+  MCContext &Ctx;
+
+  AsmPrinter &Printer;
+
+public:
+  J2MCInstLower(MCContext &ctx, AsmPrinter &printer)
+      : Ctx(ctx), Printer(printer) {}
+  using MachineOperandType = MachineOperand::MachineOperandType;
+
+  void Lower(const MachineInstr *MI, MCInst &OutMI) const;
+  MCOperand LowerOperand(const MachineOperand &MO) const;
+};
+}
+
+#endif
