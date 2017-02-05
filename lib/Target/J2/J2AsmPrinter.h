c195d7f40dec4ae2534133582f1370663dbeb757
Francis Visoiu Mistrih <francisvm@yahoo.com>
2017-02-05 14:22:29 -0800

    j2: add MCInstLower
    * lib/Target/J2/CMakeLists.txt: Add J2MCInstLower.
    * lib/Target/J2/J2AsmPrinter.cpp,
    * lib/Target/J2/J2AsmPrinter.h: Use InstLower to lower instructions to
      MCInst and stream them.
    * lib/Target/J2/J2MCInstLower.cpp,
    * lib/Target/J2/J2MCInstLower.h: Lower operands.

--- lib/Target/J2/J2AsmPrinter.h
+++ lib/Target/J2/J2AsmPrinter.h
@@ -20,12 +20,13 @@
 namespace llvm {
 
 class J2AsmPrinter : public AsmPrinter {
-
 public:
   J2AsmPrinter(TargetMachine &TM, std::unique_ptr<MCStreamer> Streamer)
       : AsmPrinter(TM, std::move(Streamer)) {}
 
   StringRef getPassName() const override { return "J2 Assembly Printer"; }
+
+  void EmitInstruction(const MachineInstr *MI) override;
 
   void EmitFunctionEntryLabel() override;
 };
