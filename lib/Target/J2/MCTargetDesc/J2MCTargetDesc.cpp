228ce7c00c6e34f260e0eff0ad2fb6216f19f7c7
Francis Visoiu Mistrih <francisvm@yahoo.com>
2017-02-05 14:22:25 -0800

    j2: add RegisterInfo
    * lib/Target/J2/CMakeLists.txt,
    * lib/Target/J2/J2.td: Include register description.
    * lib/Target/J2/J2RegisterInfo.cpp,
    * lib/Target/J2/J2RegisterInfo.h: Concrete class.
    * lib/Target/J2/J2RegisterInfo.td: Register description.
    * lib/Target/J2/MCTargetDesc/J2MCTargetDesc.cpp: Register
      MCTargetRegisterInfo.

--- lib/Target/J2/MCTargetDesc/J2MCTargetDesc.cpp
+++ lib/Target/J2/MCTargetDesc/J2MCTargetDesc.cpp
@@ -20,6 +20,18 @@
 
 using namespace llvm;
 
+#define GET_REGINFO_MC_DESC
+#define GET_REGINFO_ENUM
+#include "J2GenRegisterInfo.inc"
+
+static MCRegisterInfo *createJ2MCRegisterInfo(const Triple &TT) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  InitJ2MCRegisterInfo(X, J2::PR);
+  return X;
+}
+
 extern "C" void LLVMInitializeJ2TargetMC() {
   Target *T = &getTheJ2Target();
+  // Register the MC register info.
+  TargetRegistry::RegisterMCRegInfo(*T, createJ2MCRegisterInfo);
 }
